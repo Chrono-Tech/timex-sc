4ac71a420fa010654312f4c1cb99cc234ac852bb
mdkardaev <Kardaev.74@gmail.com>
2018-04-17 11:39:52 +0500

    deployed-addresses-local

--- migrations/utils.js
+++ migrations/utils.js
@@ -2,12 +2,13 @@ const assert = require('assert')
 const path = require('path')
 const fs = require('fs')
 const { promisify } = require('util')
-const DEPLOYED_ADDRESSES_PATH = path.join(__dirname, '..', 'deployed-addresses.json')
+const DEPLOYED_ADDRESSES_PATH = path.join(__dirname, '..', 'deployed-addresses-local.json')
 
 async function saveDeployedAddresses (addresses) {
   assert(addresses, 'Addresses must be not empty object')
 
-  const json = JSON.parse(await promisify(fs.readFile)(DEPLOYED_ADDRESSES_PATH))
+  const json = await readDeployedJson()
+
   await promisify(fs.writeFile)(DEPLOYED_ADDRESSES_PATH, JSON.stringify({
     ...json,
     ...addresses
@@ -15,8 +16,20 @@ async function saveDeployedAddresses (addresses) {
 }
 
 async function getDeployedAddress (contractName) {
-  const json = JSON.parse(await promisify(fs.readFile)(DEPLOYED_ADDRESSES_PATH))
+  const json = await readDeployedJson()
   return json[contractName]
+}
+
+async function readDeployedJson () {
+  let json = {}
+  if (fs.existsSync(DEPLOYED_ADDRESSES_PATH)) {
+    try {
+      json = JSON.parse(await promisify(fs.readFile)(DEPLOYED_ADDRESSES_PATH))
+    } catch (e) {
+      // nothiheng
+    }
+  }
+  return json
 }
 
 module.exports = {
