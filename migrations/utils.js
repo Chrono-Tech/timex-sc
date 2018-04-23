a38b3842780745c58cbc5d44fd0e795082ad6abf
mdkardaev <Kardaev.74@gmail.com>
2018-04-23 12:23:12 +0500

    refact migrate process

--- migrations/utils.js
+++ migrations/utils.js
@@ -2,7 +2,7 @@ const assert = require('assert')
 const path = require('path')
 const fs = require('fs')
 const { promisify } = require('util')
-const DEPLOYED_ADDRESSES_PATH = path.join(__dirname, '..', 'deployed-addresses-local.json')
+const DEPLOYED_ADDRESSES_PATH = path.join(__dirname, '..', 'deployed-addresses.json')
 
 async function saveDeployedAddresses (addresses) {
   assert(addresses, 'Addresses must be not empty object')
