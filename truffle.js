e826ae04cfda2c4b6b5ce6be1cc71329616c5d97
mdkardaev <Kardaev.74@gmail.com>
2018-07-31 09:49:06 +0500

    add kovan to truffle.js

--- truffle.js
+++ truffle.js
@@ -11,34 +11,19 @@ module.exports = {
     }
   },
   networks: {
-    live: {
-      network_id: 1 // Ethereum public network
-      // optional config values
-      // host - defaults to 'localhost'
-      // port - defaults to 8545
-      // gas
-      // gasPrice
-      // from - default address to use for any transaction Truffle makes during migrations
-    },
-    ropsten: {
-      provider: function () {
-        return new HDWalletProvider(mnemonic, 'https://ropsten.infura.io')
-      },
-      network_id: 3
-    },
-    demo: {
-      provider: function () {
-        return new HDWalletProvider(mnemonic, 'http://192.168.10.6:8545')
-      },
-      network_id: 135744, // 0x21240
-      gas: 4700000,
-      gasPrice: 0x01
-    },
     stage: {
       provider: function () {
         return new HDWalletProvider(mnemonic, 'https://parity.tp.ntr1x.com:8545')
       },
-      network_id: 88, // 0x21240
+      network_id: 88,
+      gas: 5000000,
+      gasPrice: 1000000000
+    },
+    kovan: {
+      provider: function () {
+        return new HDWalletProvider(mnemonic, 'https://kovan.infura.io/V7bcR20F3X5Kyg7GBH2M')
+      },
+      network_id: 42,
       gas: 4700000,
       gasPrice: 0x01
     },
@@ -46,19 +31,12 @@ module.exports = {
       provider: function () {
         return new HDWalletProvider(mnemonic, 'http://127.0.0.1:7545')
       },
-      network_id: 5777, // 0x21240
+      network_id: 5777,
       gas: 4700000,
       gasPrice: 0x01
     },
     testrpc: {
       network_id: 'default'
-    },
-    coverage: {
-      host: 'localhost',
-      network_id: '*',
-      port: 8555,
-      gas: 0xfffffffffff,
-      gasPrice: 0x01
     }
   }
 }
